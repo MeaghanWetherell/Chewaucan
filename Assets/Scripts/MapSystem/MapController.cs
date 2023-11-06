97035ff29f6183d727e556b32cc0e3e7e3c4dbf2
EllieVM <87343586+EllieVM@users.noreply.github.com>
2023-11-06 16:47:30 -0700

    fixed player position tracking, and can press 0 to hide/unhide the map

--- Assets/Scripts/MapSystem/MapController.cs
+++ Assets/Scripts/MapSystem/MapController.cs
@@ -8,19 +8,17 @@ using UnityEngine.UIElements;
 public class MapController : MonoBehaviour
 {
     public InputActionReference mapRef;
+    public InputActionReference hideMapRef;
+
     private VisualElement _root;
     private bool IsMapOpen => _root.ClassListContains("root-container-full");
 
     public GameObject Player;
-    [Range(-5, 5)]
-    public float miniMultiplyerX = -1.1f;
-    [Range(-5, 5)]
-    public float fullMultiplyerX = -1.1f;
 
-    [Range(-5, 5)]
-    public float miniMultiplyerY = -1.6f;
-    [Range(-5, 5)]
-    public float fullMultiplyerY = -1.6f;
+    [Range(0, 350)]
+    public float differenceX = 275;
+    [Range(0, 350)]
+    public float differenceY = 265;
 
     private VisualElement _playerRepresentation;
 
@@ -38,15 +36,15 @@ public class MapController : MonoBehaviour
 
     private void LateUpdate()
     {
-        var multiplierX = IsMapOpen ? fullMultiplyerX : miniMultiplyerX;
-        var multiplierY = IsMapOpen ? fullMultiplyerY : miniMultiplyerY;
-
-        _playerRepresentation.style.translate = new Translate((Player.transform.position.x * multiplierX),
-        (Player.transform.position.z * -multiplierY), 0);
-
+        //sets the player icon to a position representative of world position
         _playerRepresentation.style.rotate = new Rotate(new Angle(Player.transform.rotation.eulerAngles.y));
 
-        
+        Length x = new Length((Player.transform.position.x) - differenceX, LengthUnit.Pixel);
+        Length y = new Length((-Player.transform.position.z) + differenceY, LengthUnit.Pixel);
+
+        _playerRepresentation.style.translate = new Translate(x, y, 0);
+
+        //moves the minimap view along with the player
         if (!IsMapOpen)
         {
             var clampWidth = _mapImage.worldBound.width / 2 -
@@ -54,12 +52,10 @@ public class MapController : MonoBehaviour
             var clampHeight = _mapImage.worldBound.height / 2 -
                 _mapContainer.worldBound.height / 2;
 
-            var xPos = Mathf.Clamp((Player.transform.position.x * multiplierX),
+            var xPos = Mathf.Clamp((Player.transform.position.x) - differenceX,
                 -clampWidth, clampWidth);
-            var yPos = Mathf.Clamp((Player.transform.position.z * -multiplierY),
+            var yPos = Mathf.Clamp((-Player.transform.position.z) + differenceY,
                 -clampHeight, clampHeight);
-
-            Debug.Log("X: "+xPos+", "+yPos);
 
             _mapImage.style.translate = new Translate(xPos*-1, yPos*-1, 0);
         }
@@ -72,16 +68,26 @@ public class MapController : MonoBehaviour
     private void OnEnable()
     {
         mapRef.action.performed += (InputAction.CallbackContext context) => { ToggleMap(!IsMapOpen); };
+        hideMapRef.action.performed += (InputAction.CallbackContext context) => { DisableMap(); };
     }
 
     private void OnDisable()
     {
         mapRef.action.performed -= (InputAction.CallbackContext context) => { ToggleMap(!IsMapOpen); };
+        hideMapRef.action.performed -= (InputAction.CallbackContext context) => { DisableMap(); };
     }
 
+    //press M to switch between a full and mini map view
     void ToggleMap(bool mode)
     {
         _root.EnableInClassList("root-container-mini", !mode);
         _root.EnableInClassList("root-container-full", mode);
+    }
+
+    //press 0 to hide the map in either view
+    void DisableMap()
+    {
+        var display = _root.style.display == DisplayStyle.Flex ? DisplayStyle.None : DisplayStyle.Flex;
+        _root.style.display = display;
     }
 }
