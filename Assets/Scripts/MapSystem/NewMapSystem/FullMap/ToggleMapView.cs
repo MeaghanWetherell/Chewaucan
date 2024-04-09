1919d86937d9cc25e350cd489eed020b3ebfe91c
EllieVM <87343586+EllieVM@users.noreply.github.com>
2024-04-09 12:36:50 -0700

    added some comments

--- Assets/Scripts/MapSystem/NewMapSystem/FullMap/ToggleMapView.cs
+++ Assets/Scripts/MapSystem/NewMapSystem/FullMap/ToggleMapView.cs
@@ -15,25 +15,30 @@ public class ToggleMapView : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        // by default, we view the modern map
+        //      might change this later to view the map of the scene we were in
         modernMapView.SetActive(true);
         pleistoceceMapView.SetActive(false);
         mapName.text = "Modern Map";
     }
 
+    // enables/disables game objects so we view the map we are currently not seeing
     public void SwitchMapView()
     {
-        if (modernMapView.activeInHierarchy)
+        if (modernMapView.activeInHierarchy) // switch to view the pleistocene map
         {
             modernMapView.SetActive(false);
             pleistoceceMapView.SetActive(true);
             mapName.text = "Pleistocene Map";
         }
-        else if (pleistoceceMapView.activeInHierarchy)
+        else if (pleistoceceMapView.activeInHierarchy) // switch to view the modern map
         {
             modernMapView.SetActive(true);
             pleistoceceMapView.SetActive(false);
             mapName.text = "Modern Map";
         }
+
+        // if the teleport menu is open, close it
         if (teleportSidebarAnimator.GetBool("active") == true)
         {
             teleportSidebarAnimator.SetBool("active", false);
