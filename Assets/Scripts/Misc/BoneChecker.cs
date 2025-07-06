914a7c0f1eff236203dcb9877665a0de64c38254
MeaghanWetherell <123985064+MeaghanWetherell@users.noreply.github.com>
2025-07-06 10:35:27 -0700

    bone scene additional text options

--- Assets/Scripts/Misc/BoneChecker.cs
+++ Assets/Scripts/Misc/BoneChecker.cs
@@ -16,19 +16,62 @@ public class BoneChecker : MonoBehaviour
     
     public TextMeshProUGUI text;
 
+    private Quaternion initialRotation; // store the starting rotation
+
     private void Start()
     {
         viewer.transform.eulerAngles = new Vector3(0, Random.Range(0,360), 0);
+
+        // Save the initial rotation
+        initialRotation = viewer.transform.rotation;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (isCorrect && (viewer.transform.rotation.eulerAngles - mBoneViewer.transform.rotation.eulerAngles).magnitude < 10f)
-        {
-            text.text = "It's a little off, but this looks about right.";
-            QuestManager.questManager.GETNode("bonepile").AddCount(0);
-            this.enabled = false;
+
+        var rotationDifference = (viewer.transform.rotation.eulerAngles - mBoneViewer.transform.rotation.eulerAngles).magnitude;
+        float playerRotationAmount = Quaternion.Angle(initialRotation, viewer.transform.rotation);
+
+        //If it is correct
+        if (isCorrect) {
+
+            //player rotated it at all
+            if (playerRotationAmount >1f) 
+            {
+                //and the rotation is right
+                if (rotationDifference < 10f)
+                {
+                    text.text = "That's it! Your bone is the distal end of a femur.";
+                    QuestManager.questManager.GETNode("bonepile").AddCount(0);
+                    this.enabled = false;
+
+                } else
+                {
+                    text.text = "This looks like the right bone, but not quite the right position.";
+                }
+                   
+            } else    
+            {
+                // player hasn't rotated it at all
+                text.text = "Rotate the bone to see if it matches yours.";
+            }
+
+        } else {
+          
+
+            //incorrect bone, but the player has rotated it sufficiently
+            if (playerRotationAmount > 30f)
+            {
+                text.text = "Hmm, this doesn't seem to be the right bone. ";
+            }
+            else
+            {
+                text.text = "Rotate the bone to see if it matches yours.";
+            }
+
+
         }
+
     }
 }
