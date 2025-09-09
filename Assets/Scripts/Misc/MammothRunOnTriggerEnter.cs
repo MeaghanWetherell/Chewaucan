418f272499ec7f58f0c9459931160c0e42657fea
Zero497 <crscott1@arizona.edu>
2025-09-09 11:34:08 -0700

    j6

---
+++ Assets/Scripts/Misc/MammothRunOnTriggerEnter.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ScriptTags;
+
+public class MammothRunOnTriggerEnter : MonoBehaviour
+{
+    public Rigidbody mammothBody;
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<Player>() != null)
+        {
+            Run();
+        }
+    }
+
+    private void Run()
+    {
+        mammothBody.velocity = new Vector3(50, 0, 0);
+    }
+}
